ea7afcd1f2aa55da1396f96e311cac7351a422b0
Ian Fitzpatrick <itfitzpatrick@gmail.com>
2016-08-15 16:12:16 -0700

    Remove print statements

--- rill/server.py
+++ rill/server.py
@@ -38,18 +38,15 @@ class WebSocketRuntimeApplication(geventwebsocket.WebSocketApplication):
         return 'noflo'
 
     def on_open(self):
-        print 'im open yo'
         self.client = Client(self.on_response)
         self.client.connect("tcp://localhost", 5556)
 
     def on_close(self, reason):
-        print('im closed', reason)
         self.client.disconnect()
         self.client = None
 
     def on_message(self, message, **kwargs):
         self.logger.debug('INCOMING: {}'.format(message))
-        print('got message', message)
         if message:
             self.client.send(Message(**json.loads(message)))
 
@@ -58,12 +55,22 @@ class WebSocketRuntimeApplication(geventwebsocket.WebSocketApplication):
         self.ws.send(json.dumps(msg.to_dict()))
 
 
-def websocket_application_task():
+def websocket_application_task(host, port):
     """
     This greenlet runs the websocket server that responds to remote commands
     that inspect/manipulate the Runtime.
     """
 
+    address = 'ws://{}:{:d}'.format(host, port)
+
+    print('Runtime listening at {}'.format(address))
+    r = geventwebsocket.Resource(
+        OrderedDict([('/', WebSocketRuntimeApplication)]))
+    server = geventwebsocket.WebSocketServer(('', port), r)
+    server.serve_forever()
+
+
+def main():
     argp = argparse.ArgumentParser(
         description='Rill websocket server forwards messages through zermq '
                     'to rill runtime')
@@ -75,20 +82,10 @@ def websocket_application_task():
         help='Listen port for websocket (default: %(port)d)' % DEFAULTS)
 
     args = argp.parse_args()
-
     host = args.host
     port = args.port
-    address = 'ws://{}:{:d}'.format(host, port)
 
-    print('Runtime listening at {}'.format(address))
-    r = geventwebsocket.Resource(
-        OrderedDict([('/', WebSocketRuntimeApplication)]))
-    server = geventwebsocket.WebSocketServer(('', port), r)
-    server.serve_forever()
-
-
-def main():
-    gevent.wait([gevent.spawn(websocket_application_task)])
+    gevent.wait([gevent.spawn(lambda: websocket_application_task(host, port))])
 
 if __name__ == "__main__":
     main()
